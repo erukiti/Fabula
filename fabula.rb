9d64c66c0f6b7253e17ec9b5a03289d400f7d9db
User <ele@home-server01.(none)>
2011-06-14 00:26:58 +0900

    リファクタリングとか

---
+++ fabula.rb
@@ -0,0 +1,330 @@
+#! /usr/bin/env ruby
+
+$KCODE = 'UTF8'
+
+require 'rexml/document'
+require 'yaml'
+require 'fileutils'
+
+class EPG
+  attr_reader :program_list
+  attr_reader :channel_list
+
+  def initialize(initializer, opt = nil)
+    init = initializer.new(opt)
+    @program_list = init.program_list
+  end
+
+  def update(epg_updater)
+    # epg_updater に含まれている時間の範囲を算出する
+
+    start = nil
+    stop = nil
+    epg_updater.program_list.each { |program|
+      start = program.start if start.nil? || start > program.start
+      stop = program.stop if stop.nil? || stop < program.stop
+    }
+
+    # 新しい program_list を作成する
+    # @program_list の start 以前のものを抽出
+    new_program_list = @program_list.select{ |program| program.stop <= start } +
+                       epg_updater.program_list +
+                       @program_list.select{ |program| stop <= program.start}
+
+   @program_list = new_program_list
+  end
+
+end
+
+class EPGFromNull
+  def initialize(opt)
+  end
+  
+  def channel_list
+    []
+  end
+  
+  def program_list
+    []
+  end
+end
+
+class EPGFromFile
+  attr_reader :channel_list
+  attr_reader :program_list
+
+  def initialize(savedata)
+    epgdata = YAML.load(savedata)
+    @program_list = []
+    if epgdata
+      @channel_list = epgdata[:channel]
+      epgdata[:program].each { |program_a|
+        @program_list << Program.new(program_a)
+      }
+    else
+      @channel_list = []
+    end
+  end
+end
+
+# FIXME: EPGFromFile と統合できる？
+class EPGToFile
+  def EPGToFile.to_yaml(epg)
+    program_a = []
+    epg.program_list.each { |program|
+      program_a << program.to_a
+    }
+
+    {:channel => epg.channel_list, :program => program_a}.to_yaml
+  end
+end
+
+class EPGFromEpgdump
+  def initialize(epgxml)
+    epgdata = REXML::Document.new epgxml
+
+    @channel_list = {}
+    epgdata.elements.each('tv/channel') { |ch|
+      @channel_list[ch.attribute('id').to_s] = ch.elements['display-name'].text
+    }
+
+    @program_list = []
+    epgdata.elements.each('tv/programme') { |programme|
+      @program_list << Program.new(
+        :channel => programme.attribute('channel').to_s,
+        :title => programme.elements['title'].text,
+        :desc => programme.elements['desc'].text,
+        :category => programme.elements["category[@lang='en']"].text,
+        :start => EPGFromEpgdump.time_from_epgdump(programme.attribute('start').to_s),
+        :stop => EPGFromEpgdump.time_from_epgdump(programme.attribute('stop').to_s)
+      )
+    }
+  end
+
+  def channel_list
+    @channel_list
+  end
+  
+  def program_list
+    @program_list
+  end
+
+  def EPGFromEpgdump.time_from_epgdump(epgdump_time)
+    #ひとまずタイムゾーンは +9000 に決めうち
+    #2011060302400 +0900
+    Time.mktime(epgdump_time[0..3], epgdump_time[4..5], epgdump_time[6..7], epgdump_time[8..9], epgdump_time[10..11], epgdump_time[12..13])
+    
+  end
+
+end
+
+class Program
+  attr_reader :channel
+  attr_reader :title
+  attr_reader :desc
+  attr_reader :category
+  attr_reader :start
+  attr_reader :stop
+
+  attr_accessor :slot
+  attr_accessor :conflict
+  attr_accessor :priority
+
+  def initialize(data)
+    @channel = data[:channel]
+    @title = data[:title]
+    @desc = data[:desc]
+    @category = data[:category]
+    @start = data[:start]
+    @stop = data[:stop]
+
+    @slot = nil
+    @conflict = false
+  #  @priority = nil
+  end
+
+  def to_a
+    {
+      :channel => @channel,
+      :title => @title,
+      :desc => @desc,
+      :category => @category,
+      :start => @start,
+      :stop => @stop,
+      :slot => @slot,
+      :conflict => @conflict,
+    }
+  end
+end
+
+
+
+class Fabula
+  attr_reader :epg
+
+  def initialize()
+    @channel = []
+    @epg = EPG.new(EPGFromNull)
+    @accessor = FabulaAccessor.new
+    @temporary = nil
+  end
+
+  def injection_accessor(accessor)
+    @accessor = accessor.new
+  end
+
+  def injection_config(config)
+    @channel = config[:channel]
+    @temporary = config[:temporary]
+  end
+
+  def load_config
+    # FIXME: ファイルがないときの処理
+    config_data = YAML.load(IO.read('.config.yaml'))
+    @channel = config_data[:channel]
+    @accessor.temporary = config_data[:temporary]
+    # FIXME: temporary directory 処理
+  end
+
+  def load_order
+    #begin
+      FileUtils.touch('.order.yaml')
+
+      @order_file = File.open('.order.yaml', 'r+')
+      unless @order_file.flock(File::LOCK_EX | File::LOCK_NB)
+        # ロックを取得できないので今回の実行は諦める
+        # FIXME: log出力
+        return false
+      end
+
+      @epg = EPG.new(EPGFromFile, @order_file.read)
+    #rescue
+    #  print "-------- error\n"
+    #  p $!
+    #  print "-------- error\n"
+
+    #end
+  end
+
+  def save_order
+    @order_file.rewind
+    @order_file << EPGToFile.to_yaml(epg)
+  end
+
+  def load_epgdump
+    Dir.glob("#{@accessor.temporary}/*_epg.xml") { |filename|
+print "!epgdump file: #{filename}\n"
+    }
+  end
+
+
+
+  def minutely
+    load_config
+    load_order
+    discovery_epg(true) if @epg.program_list.empty?
+    save_order
+  end
+
+  def discovery_epg(is_short)
+    @epg = @accessor.discovery_epg(@channel, is_short)
+  end
+
+end
+
+class FabulaAccessor
+  def initialize
+    
+  end
+
+  def get_filename(ch)
+    # ch が被ってて、かつ同時アクセスがあるとアウト
+    # デバイスの排他制御でなんとかなるはず
+
+    time = Time.now.strftime("%Y%m%d%H%M%S")
+    ["#{@temporary}/#{ch}_#{time}.ts", "#{@temporary}/#{ch}_#{time}_epg.xml"]
+  end
+
+  def get_epg(ch, is_short)
+    ts_name, epg_name = get_filename(ch)
+    epgtemp_name = "#{epg_name}_progress"
+    sec = is_short ? 3 : 60
+
+    `recpt1 #{ch} #{sec} #{ts_name}`
+    dump = `epgdump #{ch} #{ts_name} -`
+
+    # FIXME: stderr からログを取得するようにする (特に recpt1)
+    # FIXME: それぞれのコマンドが失敗したら false を返すようにする
+
+    EPG.new(EPGFromEpgdump, dump)
+  end
+
+  def discovery_epg(channel, is_short)
+print "discovery\n"
+    epg = EPG.new(EPGFromNull)
+
+    channel.each { |ch, name|
+      epg.update(get_epg(ch, is_short))
+      # FIXME: epg 取得時に失敗した場合何回かリトライしてみる？
+    }
+
+    epg
+  end
+
+  def temporary=(temporary)
+    @temporary = temporary
+  end
+
+end
+
+class ControlList
+  def initialize
+    @control_list = []
+  end
+
+  def load_control_list
+  end
+
+  def program_mapper(program)
+    program.priority = 0
+    @control_list.each {|control|
+      next if control['title'] != nil && Regexp.new(control['title']) !~ program.title
+      next if control['desc'] != nil && Regexp.new(control['desc']) !~ program.desc
+      next if control['category'] != nil && control['category'] != program.category
+      next if control['channel'] != nil && control['channel'] != program.channel
+
+      if control['time_range'] != nil
+        next if control['time_range'].last <= program.start
+        # FIXME: 前後両方が完全に収まってる事をチェックする
+
+
+        #program.start = control['time_range'].first if program.start < control['time_range'].first
+        #program.stop = control['time_range'].last if program.stop > control['time_range'].last
+        # もし中途半端な時間の録画を行う場合、別アトリビュートを用意すべき
+      end
+
+      # すべてマッチしたので、priority を変更
+
+      # 一度ブラックリスト(優先度:-1)のチェックがついてしまった場合、優先度は変更できなくなる
+      next if program.priority < 0
+
+      # blacklist or 優先度上昇
+      program.priority = control['priority'] if control['priority'] < 0 || control['priority'] > program.priority
+    }
+
+    program
+  end
+
+  def add(title, desc, category, time_range, channel, priority)
+    @control_list << {'title' => title, 'desc' => desc, 'category' => category, 'time_range' => time_range, 'channel' => channel, 'priority' => priority}
+    self
+  end
+
+end
+
+# program_list = epg.program_list.map { |program| cl.program_mapper(program)}
+# FIXME map! に動作をかえる？
+
+
+#fabula = Fabula.new
+#fabula.minutely
---
+++ test_fabula.rb
@@ -0,0 +1,346 @@
+#! /usr/bin/env ruby
+
+$KCODE = 'UTF8'
+
+require 'test/unit'
+require 'fabula.rb'
+
+class TC_EPGFromEpgdump < Test::Unit::TestCase
+  def test_time_from_epgdump
+    
+    tm = EPGFromEpgdump.time_from_epgdump("20110603024000 +0900")
+    assert_equal(tm, Time.mktime(2011, 6, 3, 2, 40, 0))
+  end 
+
+  def test_initialize
+    dummy_xml = <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE tv SYSTEM "xmltv.dtd">
+
+<tv generator-info-name="tsEPG2xml" generator-info-url="http://localhost/">
+  <channel id="C39">
+    <display-name lang="ja_JP">テレビ東京１</display-name>
+  </channel>
+  <programme start="20110603024500 +0900" stop="20110603031500 +0900" channel="C39">
+    <title lang="ja_JP">まりあほりっくあらいぶ</title>
+    <desc lang="ja_JP">「早熟の婚約者」 ちょっと待って！私が主人公なのよっ！なのに…なのにこんな扱いって無いんじゃないっ！？んっ。しかもこのちんちくりん、私のこと無視しよった！</desc>
+    <category lang="ja_JP">アニメ・特撮</category>
+    <category lang="en">anime</category>
+  </programme>
+  <programme start="20110603031500 +0900" stop="20110603033000 +0900" channel="C39">
+    <title lang="ja_JP">Ａ×Ａ</title>
+    <desc lang="ja_JP">（ダブルエー）「フィギュアスケートＪａｐａｎ　Ｏｐｅｎ２０１１」 国と地域の威信をかけて頂点を目指すチーム戦！「フィギュアスケートＪａｐａｎ　Ｏｐｅｎ２０１１」をご紹介！日本代表の活躍の歴史を名場面とともに振り返ります！</desc>
+    <category lang="ja_JP">情報</category>
+    <category lang="en">information</category>
+  </programme>
+  <programme start="20110603033000 +0900" stop="20110603040000 +0900" channel="C39">
+    <title lang="ja_JP">続　夏目友人帳第９話</title>
+    <desc lang="ja_JP">「桜並木の彼」 妖（あやかし）を見ることができる少年・夏目貴志と、招き猫の姿をした妖・ニャンコ先生が繰り広げる、妖しく、切なく、そして懐かしい物語。</desc>
+    <category lang="ja_JP">アニメ・特撮</category>
+    <category lang="en">anime</category>
+  </programme>
+</tv>
+EOF
+    from_epgdump = EPGFromEpgdump.new(dummy_xml)
+    assert_equal(from_epgdump.program_list.size, 3)
+  end
+end
+
+
+class DummyEPG
+  def initialize(opt)
+  end
+  def program_list
+    [Program.new(
+      :category => "anime", 
+      :title => "ほげ", 
+      :start => Time.local(2011, 6, 3, 2, 45), 
+      :stop => Time.local(2011, 6, 3, 3, 15), 
+      :desc => "ほげですく", 
+      :channel => 'C39'
+    ), Program.new(
+      :category => "infomation", 
+      :title => "ふが", 
+      :stop => Time.local(2011, 6, 3, 3, 30), 
+      :start => Time.local(2011, 6, 3, 3, 15), 
+      :desc => "ふがですく", 
+      :channel => 'C39'
+    )]
+  end
+
+  def channel_list
+    {"C39"=>"テレビ東京１"}
+  end
+end
+
+class DummyEPG2
+  def initialize(opt)
+  end
+  def program_list
+    [Program.new(
+      :category => "anime", 
+      :title => "hoge/1", 
+      :start => Time.local(2011, 6, 3, 2, 45), 
+      :stop => Time.local(2011, 6, 3, 3, 0), 
+      :desc => "hoge1", 
+      :channel => 'C39'
+    ), Program.new(
+      :category => "anime", 
+      :title => "hoge/2", 
+      :start => Time.local(2011, 6, 3, 3, 0), 
+      :stop => Time.local(2011, 6, 3, 3, 15), 
+      :desc => "hoge2", 
+      :channel => 'C39'
+    )]
+  end
+
+  def channel_list
+    {"C39"=>"テレビ東京１"}
+  end
+end
+
+class DummyAccessor
+  def initialize
+    @epg = EPG.new(DummyEPG)
+  end
+  def get_epg(ch, is_short)
+    @epg if ch == 'C39'
+  end
+  def discovery_epg(channel, is_short)
+    channel.each { |ch, name|
+      @epg.update(get_epg(ch, is_short))
+      # FIXME: epg 取得時に失敗した場合何回かリトライしてみる？
+    }
+
+    @epg
+  end
+end
+
+class DummyAccessor2
+  def initialize
+    @epg = EPG.new(EPGFromNull)
+  end
+  def get_epg(ch, is_short)
+    @epg
+  end
+  def discovery_epg(channel, is_short)
+    channel.each { |ch, name|
+      @epg.update(get_epg(ch, is_short))
+      # FIXME: epg 取得時に失敗した場合何回かリトライしてみる？
+    }
+
+    @epg
+  end
+end
+
+class TC_EPG < Test::Unit::TestCase
+  def test_program
+    # 一応 dummy データに問題がない事を確認
+    epg = EPG.new(DummyEPG)
+    assert_equal(epg.program_list.size, 2)
+    assert_equal(epg.program_list[0].category, "anime")
+    assert_equal(epg.program_list[0].title, "ほげ")
+    assert_equal(epg.program_list[0].start, Time.local(2011, 6, 3, 2, 45))
+    assert_equal(epg.program_list[0].stop, Time.local(2011, 6, 3, 3, 15))
+    assert_equal(epg.program_list[0].desc, "ほげですく")
+    assert_equal(epg.program_list[0].channel, "C39")
+    assert_equal(epg.program_list[1].category, "infomation")
+    assert_equal(epg.program_list[1].title, "ふが")
+    assert_equal(epg.program_list[1].start, Time.local(2011, 6, 3, 3, 15))
+    assert_equal(epg.program_list[1].stop, Time.local(2011, 6, 3, 3, 30))
+  end
+
+  def test_update
+    epg = EPG.new(DummyEPG)
+    epg2 = EPG.new(DummyEPG2)
+    epg.update(epg2)
+    assert_equal(epg.program_list.size, 3)
+    assert_equal(epg.program_list[0].category, "anime")
+    assert_equal(epg.program_list[0].title, "hoge/1")
+    assert_equal(epg.program_list[0].start, Time.local(2011, 6, 3, 2, 45))
+    assert_equal(epg.program_list[0].stop, Time.local(2011, 6, 3, 3, 0))
+    assert_equal(epg.program_list[1].category, "anime")
+    assert_equal(epg.program_list[1].title, "hoge/2")
+    assert_equal(epg.program_list[1].start, Time.local(2011, 6, 3, 3, 0))
+    assert_equal(epg.program_list[1].stop, Time.local(2011, 6, 3, 3, 15))
+    assert_equal(epg.program_list[2].category, "infomation")
+    assert_equal(epg.program_list[2].title, "ふが")
+    assert_equal(epg.program_list[2].start, Time.local(2011, 6, 3, 3, 15))
+    assert_equal(epg.program_list[2].stop, Time.local(2011, 6, 3, 3, 30))
+  end
+
+  def test_minutely
+    fabula = Fabula.new
+    fabula.minutely
+  end
+
+  def test_discovery_epg
+    fabula = Fabula.new
+    fabula.injection_config(:channel => {'C39' => 'テレビ東京'})
+    fabula.injection_accessor(DummyAccessor2)
+    fabula.discovery_epg(true)
+    epg = fabula.epg
+    assert_equal(epg.program_list.size, 0)
+
+    fabula = Fabula.new
+    fabula.injection_config(:channel => {'C39' => 'テレビ東京'})
+    fabula.injection_accessor(DummyAccessor)
+    fabula.discovery_epg(true)
+    epg = fabula.epg
+    assert_equal(epg.program_list.size, 2)
+    assert_equal(epg.program_list[0].category, "anime")
+    assert_equal(epg.program_list[0].title, "ほげ")
+    assert_equal(epg.program_list[0].start, Time.local(2011, 6, 3, 2, 45))
+    assert_equal(epg.program_list[0].stop, Time.local(2011, 6, 3, 3, 15))
+    assert_equal(epg.program_list[0].desc, "ほげですく")
+    assert_equal(epg.program_list[0].channel, "C39")
+    assert_equal(epg.program_list[1].category, "infomation")
+    assert_equal(epg.program_list[1].title, "ふが")
+    assert_equal(epg.program_list[1].start, Time.local(2011, 6, 3, 3, 15))
+    assert_equal(epg.program_list[1].stop, Time.local(2011, 6, 3, 3, 30))
+
+  end
+
+  def test_program_map
+  
+    epg = EPG.new(DummyEPG)
+
+    # タイトル一致 (完全一致)
+    cl = ControlList.new.add("ほげ", nil, nil, nil, nil, 1)
+    prog = epg.program_list.map { |program| cl.program_mapper(program)}
+    assert_instance_of(Array, prog)
+    assert_instance_of(Program, prog[0])
+    assert_equal(prog[0].priority, 1)
+    assert_equal(prog[1].priority, 0)
+    cl = nil
+
+    # タイトル一致 (正規表現/部分一致)
+    cl = ControlList.new.add("ほ", nil, nil, nil, nil, 1)
+    prog = epg.program_list.map { |program| cl.program_mapper(program)}
+    assert_equal(prog[0].priority, 1)
+    assert_equal(prog[1].priority, 0)
+    cl = nil
+
+    # タイトル一致 (正規表現)
+    cl = ControlList.new.add("ふが.*", nil, nil, nil, nil, 1)
+    prog = epg.program_list.map { |program| cl.program_mapper(program)}
+    assert_equal(prog[0].priority, 0)
+    assert_equal(prog[1].priority, 1)
+    cl = nil
+
+    # タイトル一致 (一致できない)
+    cl = ControlList.new.add("ほげぼ", nil, nil, nil, nil, 1)
+    prog = epg.program_list.map { |program| cl.program_mapper(program)}
+    assert_equal(prog[0].priority, 0)
+    assert_equal(prog[1].priority, 0)
+    cl = nil
+
+    # 説明一致 (完全一致)
+    cl = ControlList.new.add(nil, "ほげですく", nil, nil, nil, 1)
+    prog = epg.program_list.map { |program| cl.program_mapper(program)}
+    assert_equal(prog[0].priority, 1)
+    assert_equal(prog[1].priority, 0)
+    cl = nil
+
+    # 説明一致 (正規表現/部分一致)
+    cl = ControlList.new.add(nil, "ですく", nil, nil, nil, 1)
+    prog = epg.program_list.map { |program| cl.program_mapper(program)}
+    assert_equal(prog[0].priority, 1)
+    assert_equal(prog[1].priority, 1)
+    cl = nil
+
+    # 説明一致 (正規表現)
+    cl = ControlList.new.add(nil, "ふ.*で", nil, nil, nil, 1)
+    prog = epg.program_list.map { |program| cl.program_mapper(program)}
+    assert_equal(prog[0].priority, 0)
+    assert_equal(prog[1].priority, 1)
+    cl = nil
+
+    # カテゴリ一致
+    cl = ControlList.new.add(nil, nil, "anime", nil, nil, 1)
+    prog = epg.program_list.map { |program| cl.program_mapper(program)}
+    assert_equal(prog[0].priority, 1)
+    assert_equal(prog[1].priority, 0)
+    cl = nil
+
+    # 時間指定 (範囲に入ってる | 入ってない)
+    cl = ControlList.new.add(nil, nil, nil, Time.local(2011, 6, 3, 2, 45) ... Time.local(2011, 6, 3, 3, 15), nil, 1)
+    prog = epg.program_list.map { |program| cl.program_mapper(program)}
+    assert_equal(prog[0].priority, 1)
+    assert_equal(prog[1].priority, 0)
+    cl = nil
+
+    # チャンネル不一致
+    cl = ControlList.new.add(nil, nil, nil, nil, "C38", 1)
+    prog = epg.program_list.map { |program| cl.program_mapper(program)}
+    assert_equal(prog[0].priority, 0)
+    assert_equal(prog[1].priority, 0)
+    cl = nil
+
+    # チャンネル一致
+    cl = ControlList.new.add(nil, nil, nil, nil, "C39", 1)
+    prog = epg.program_list.map { |program| cl.program_mapper(program)}
+    assert_equal(prog[0].priority, 1)
+    assert_equal(prog[1].priority, 1)
+    cl = nil
+
+    # 複数指定
+    cl = ControlList.new.add("ほげ", nil, nil, nil, nil, 1).add("ふが", nil, nil, nil, nil, 2)
+    prog = epg.program_list.map { |program| cl.program_mapper(program)}
+    assert_equal(prog[0].priority, 1)
+    assert_equal(prog[1].priority, 2)
+    cl = nil
+
+    # 複数指定
+    cl = ControlList.new.add("ほげ", nil, nil, nil, nil, 1).add(nil, "ほげ", nil, nil, nil, 2)
+    prog = epg.program_list.map { |program| cl.program_mapper(program)}
+    assert_equal(prog[0].priority, 2)
+    assert_equal(prog[1].priority, 0)
+    cl = nil
+
+    # 複数指定
+    cl = ControlList.new.add("ほげ", nil, nil, nil, nil, 2).add(nil, "ほげ", nil, nil, nil, 1)
+    prog = epg.program_list.map { |program| cl.program_mapper(program)}
+    assert_equal(prog[0].priority, 2)
+    assert_equal(prog[1].priority, 0)
+    cl = nil
+
+    # 複数指定
+    cl = ControlList.new.add("ほげ", nil, nil, nil, nil, -1).add(nil, "ほげ", nil, nil, nil, 1)
+    prog = epg.program_list.map { |program| cl.program_mapper(program)}
+    assert_equal(prog[0].priority, -1)
+    assert_equal(prog[1].priority, 0)
+    cl = nil
+
+    # 複数指定
+    cl = ControlList.new.add("ほげ", nil, nil, nil, nil, 2).add(nil, "ほげ", nil, nil, nil, -1)
+    prog = epg.program_list.map { |program| cl.program_mapper(program)}
+    assert_equal(prog[0].priority, -1)
+    assert_equal(prog[1].priority, 0)
+    cl = nil
+
+  end
+
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
